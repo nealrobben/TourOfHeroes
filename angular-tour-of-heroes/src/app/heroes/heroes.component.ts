3c5d80c0fb4bb056b724b784cb95da3de27dbccd
nealrobben <neal.robben@cegeka.com>
2019-08-26 17:18:15 +0200

    Added MessageService

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes';
+
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -10,27 +10,22 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  hero:Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
+  selectedHero: Hero;
 
   heroes: Hero[];
 
-  selectedHero:Hero;
-
-  onSelect(hero:Hero):void{
-    this.selectedHero = hero;
-  }
-
-  constructor(private heroService:HeroService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  getHeroes():void{
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
